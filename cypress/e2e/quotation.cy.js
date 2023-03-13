03cae66c55a296a62e49c573514712f9efc99bdc
Andri Desmana <andridesmana29@gmail.com>
2023-03-13 14:53:21 +0700

    quotation

--- cypress/e2e/quotation.cy.js
+++ cypress/e2e/quotation.cy.js
@@ -1,6 +1,6 @@
 describe("Edit Akun&User Page SISFWD", (dashboard, button) => {
   it("Visit Login SISFWD", () => {
-    cy.visit("https://dev-fe.sisfwd.com");
+    cy.visit("https://trial.rpl.id/");
   });
   it("Contains Close X", () => {
     // close popup
@@ -15,11 +15,10 @@ describe("Edit Akun&User Page SISFWD", (dashboard, button) => {
   it("Login", () => {
     //email
     const email = cy.get("input[name='email']");
-    email.type("corporate@gmail.com");
-    cy.wait(1000);
+    email.type("rplkeysuper@gmail.com");
     //password
     const password = cy.get("input[name='password']");
-    password.type("123456");
+    password.type("rpl12345");
     //show password
     cy.get('label[for="password"]')
       .find('svg[class*="cursor-pointer"]')
@@ -27,20 +26,32 @@ describe("Edit Akun&User Page SISFWD", (dashboard, button) => {
     //masuk
     button = cy.get("button");
     button.contains("Masuk").click();
-    cy.wait(500);
+    cy.wait(1000);
   });
   it("Quotation", ()=>{
-    cy.get('[href="/customer/quotation"]').click();
+    cy.get('.z-20')
+    cy.get('.block > .w-6').click();
     cy.wait(1000);
-    cy.get(".ml-auto > .text-white").click();
-    cy.get('[name="m_service_id"]').click();
-    cy.get('#headlessui-combobox-options-22').contains('Door To Door').click();
-    cy.get('[name="origin_city_id"]').click();
-    cy.get('[aria-labelledby="headlessui-combobox-button-5"]').contains('KOTA ADM. JAKARTA BARAT').click();
-    cy.get('[name="destination_city_id"]').click();
-    cy.get('[aria-labelledby="headlessui-combobox-button-7"]').contains('KOTA ADM. JAKARTA SELATAN').click();
-    cy.get('[name="date"]').click();
-    cy.get('.rdrMonths > :nth-child(1)').contains('16').click();
+    cy.get('[href="/dashboard/quotation"]').click();
+    cy.get('[href="/dashboard/quotation/create/1"]').click();
+    cy.get('[name="customer"]').click();
+    cy.get('[role="listbox"]').contains('PT . Angsa Logistik').click();
+    cy.get('input[id="project_name"]').type('Jakarta-Medan');
+    cy.get('[name="deliveryService"]').click();
+    cy.get('[role="listbox"]').contains('Door To Door').click();
+    cy.get('[name="originCity"]').click();
+    cy.get('#headlessui-combobox-option-467').contains('KOTA ADM. JAKARTA TIMUR').click(); 
+    // cy.get('[role="listbox"]').contains('KOTA ADM. JAKARTA TIMUR');
+    // cy.wait(1000);
+    // cy.get(".ml-auto > .text-white").click();
+    // cy.get('[name="m_service_id"]').click();
+    // cy.get('#headlessui-combobox-options-22').contains('Door To Door').click();
+    // cy.get('[name="origin_city_id"]').click();
+    // cy.get('[aria-labelledby="headlessui-combobox-button-5"]').contains('KOTA ADM. JAKARTA BARAT').click();
+    // cy.get('[name="destination_city_id"]').click();
+    // cy.get('[aria-labelledby="headlessui-combobox-button-7"]').contains('KOTA ADM. JAKARTA SELATAN').click();
+    // cy.get('[name="date"]').click();
+    // cy.get('.rdrMonths > :nth-child(1)').contains('16').click();
     // cy.get('[name = "origin_city_id"]').click();
 
   });
